53dec07b9cd6b7258409e33c7e8b9b65dd559707
Maarten Balliauw <maarten@balliauw.be>
2014-04-09 11:14:15 +0200

    Initial commit

---
+++ app/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1>SearchPortal</h1>
+        <p>This is a sample application written for an article in <a href="http://www.phparch.com/">php[architect]</a></p>
+    </div>
+</div>
+@stop
